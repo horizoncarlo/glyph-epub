1ca6a0c29ee32b92d178cb8967b218c7d48f6d97
horizoncarlo <carlo.gug@gmail.com>
2026-06-25 14:21:01 -0600

    More script changes, use some defaults, pass any user params

--- kobo-chat/go-gunicorn.sh
+++ kobo-chat/go-gunicorn.sh
@@ -2,4 +2,4 @@
 # Single worker to match what we did with Flask, and also to keep any in-memory data in sync
 # Timeout for SSE since we heartbeat anyway
 # Put access and error logs to stdout, so that pm2 or screen or whatever will show it
-gunicorn -k gevent --workers 1 --timeout 120 --access-logfile - --error-logfile - -b 0.0.0.0:4646 main:app
+gunicorn -k gevent --timeout 120 --access-logfile - --error-logfile - -b 0.0.0.0:4646 main:app $@
