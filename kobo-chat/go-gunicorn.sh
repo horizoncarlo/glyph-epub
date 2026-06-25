271fc9c4c15b3d2882ecc01a05ee077005f620c0
horizoncarlo <carlo.gug@gmail.com>
2026-06-25 14:16:39 -0600

    Few more go script param tweaks

--- kobo-chat/go-gunicorn.sh
+++ kobo-chat/go-gunicorn.sh
@@ -1,6 +1,5 @@
 # Use gevent instead of --threaded, as the latter requires an exact number of active SSE sessions
-# Workers 2 to match our VPS
+# Single worker to match what we did with Flask, and also to keep any in-memory data in sync
 # Timeout for SSE since we heartbeat anyway
 # Put access and error logs to stdout, so that pm2 or screen or whatever will show it
-# Then usual binding to port and running the app
-gunicorn -k gevent --workers 2 --timeout 120 --access-logfile - --error-logfile - --log-level debug -b 0.0.0.0:4646 main:app
+gunicorn -k gevent --workers 1 --timeout 120 --access-logfile - --error-logfile - -b 0.0.0.0:4646 main:app
--- kobo-chat/go-prod.sh
+++ kobo-chat/go-prod.sh
@@ -6,5 +6,6 @@ export GLYPH_SECRET_KEY=
 export GLYPH_NTFY_TOPIC=
 
 source .venv/bin/activate
-screen -d -m -S glyphchat sh -c "flask --app main.py run -p 4646 --host=0.0.0.0 $@"
+#screen -d -m -S glyphchat sh -c "flask --app main.py run -p 4646 --host=0.0.0.0 $@"
+screen -d -m -S glyphchat sh -c "bash go-gunicorn.sh"
 screen -r
